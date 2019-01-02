f7d516af042967b73ae859acc9adf4881e4417db
ihiroshi27 <perayuththanyanon@icloud.com>
2019-01-03 06:27:59 +0700

    Redesign Menu Navigator

--- src/menu.js
+++ src/menu.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
 import './css/menu.css'
 import Logo from './img/logo.png'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { NavLink } from 'react-router-dom'
+
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = ({
-            category: "", name: "", district: "", province: ""
-        })
-    }
     render() {
-        const { name, category, district, province } = this.state
         return (
-
-            <div className="container-menu">
-                <div className="menu-left">
-                    <img className="img-logo" src={Logo} />
-                    <Link to="/" className="menu-text">Home</Link>
-                    <Link to={{ pathname: "/list", state: { category, name, district, province } }} className="menu-text">Listings</Link>
-                    <Link to="/blog" className="menu-text">Blog</Link>
-                </div>
-                <div className="menu-right">
-
+            <div id="menu">
+                <div className="menu-wrapper">
+                    <img className="logo" src={ Logo } />
+                    <div className="menu-nav">
+                        <NavLink exact to="/" activeClassName="selected">Home</NavLink>
+                        <NavLink exact to="/list" activeClassName="selected">Listing</NavLink>
+                        <NavLink exact to="/blog" activeClassName="selected">Blog</NavLink>
+                        <NavLink exact to="/contact" activeClassName="selected">Contact</NavLink>
+                    </div>
                 </div>
             </div>
         );

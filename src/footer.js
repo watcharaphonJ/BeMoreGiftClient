c8b542252b27c2783c718dd6756fab44b03d0734
unknown <watcharaphonj@kkumail.com>
2019-01-03 07:04:03 +0700

    footer

--- src/footer.js
+++ src/footer.js
@@ -5,23 +5,26 @@ export default class footer extends Component {
     render() {
         return (
             <div className="footer">
-                <div className="about box-footer">
-                    <img className="logo-footer" src={logo} alt="logo" />
-                    <div className="describe">
-                        bruh bruh bruh
+                <div className="container-footer">
+                    <div className="about box-footer">
+                        <img className="logo-footer" src={logo} alt="logo" />
+                        <div className="describe">
+                            Morbi convallis bibendum urna ut viverra. Maecenas quis consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi ultricies laoreet ullamcorper phasellus semper.
+                        </div>
                     </div>
-                </div>
-                <div className="helful box-footer">
-                    <div className="header-helpful">Helpful Links</div>
-                    test
-            </div>
-                <div className="contact box-footer">
-                    <div className="header-helpful">
-                        Contact Us
+                    { /* <div className="helful box-footer">
+                        <div className="header-helpful">Helpful Links</div>
+                        test
+            </div>*/}
+                    <div className="contact box-footer">
+                        <div className="header-helpful">
+                            Contact Us
+                        </div>
+                        <div>
+                            12345 Little Lonsdale St, Melbourne</div>
+                        E-mail : <div style={{ display: "inline-block", color: "#f91942", whiteSpace: "pre-line" }}>example@hotmail.com</div>
+                        <br />Phone : <div style={{ display: "inline-block", whiteSpace: "pre-line" }}>081-3722300</div>
                     </div>
-
-                    E-mail : <div style={{ display: "inline-block", color: "#f91942", whiteSpace: "pre-line" }}>example@hotmail.com</div>
-                    <br />Phone : <div style={{ display: "inline-block", whiteSpace: "pre-line" }}>081-3722300</div>
                 </div>
             </div>
         )
